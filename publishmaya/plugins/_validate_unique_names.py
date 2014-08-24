9ac4bf3271f971a290f551260a018b3443360c99
Marcus Ottosson <konstruktion@gmail.com>
2014-08-24 10:26:14 +0200

    Resources integrated from main publish repo

---
+++ publishmaya/plugins/_validate_unique_names.py
@@ -0,0 +1,22 @@
+import publish.backend.plugin
+
+
+class ValidateUniqueNames(publish.backend.plugin.Validator):
+
+    @property
+    def families(self):
+        return ['model', 'animation', 'animRig']
+
+    @property
+    def hosts(self):
+        return ['maya']
+
+    @property
+    def version(self):
+        return (0, 1, 0)
+
+    def process(self):
+        pass
+
+    def fix(self):
+        pass
---
+++ tests/full/monolithic_project_test/README.md
@@ -0,0 +1,3 @@
+### Monolithic
+
+A single Maya project for an entire project. Here, assets are separated only internally in sub-folders of the Maya project directory.
