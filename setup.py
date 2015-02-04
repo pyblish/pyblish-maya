35b9009effeb69357a731c56a77ccce9f48e8a68
Marcus Ottosson <konstruktion@gmail.com>
2015-02-04 10:07:58 +0000

    Adding pyblish as dependency

--- setup.py
+++ setup.py
@@ -3,15 +3,14 @@
 
 """This setup script packages pyblish_maya"""
 
+import os
+import imp
 from setuptools import setup, find_packages
 
 
 with open('README.txt') as f:
     readme = f.read()
 
-
-import os
-import imp
 
 version_file = os.path.abspath('pyblish_maya/version.py')
 version_mod = imp.load_source('version', version_file)
@@ -48,4 +47,5 @@ setup(
         'pyblish_maya': ['plugins/*.py',
                          'pythonpath/*.py']
     },
+    install_requires=["pyblish>=1.0.11"]
 )
