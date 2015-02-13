404ab61ca1a34e03391f786f5d8291540ad983d8
Marcus Ottosson <konstruktion@gmail.com>
2015-02-13 14:39:39 +0000

    Simplified service.py

--- pyblish_maya/service.py
+++ pyblish_maya/service.py
@@ -1,21 +1,22 @@
-
 # Dependencies
 import pyblish_endpoint.service
 from .version import version
 
 from maya import utils
 
+wrapper = utils.executeInMainThreadWithResult
+
 
 class MayaService(pyblish_endpoint.service.EndpointService):
     def init(self, *args, **kwargs):
-        return utils.executeInMainThreadWithResult(
-            super(MayaService, self).init, *args, **kwargs)
+        orig = super(MayaService, self).init
+        return wrapper(orig, *args, **kwargs)
 
     def next(self, *args, **kwargs):
-        return utils.executeInMainThreadWithResult(
-            super(MayaService, self).next, *args, **kwargs)
+        orig = super(MayaService, self).next
+        return wrapper(orig, *args, **kwargs)
 
     def versions(self):
         versions = super(MayaService, self).versions()
-        versions["pyblish-maya"] = version
+        versions["pyblish-nuke"] = version
         return versions
