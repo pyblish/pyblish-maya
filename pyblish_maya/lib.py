2d204a2a1dad8eea2b084ec1ce92b4be5459db56
Marcus Ottosson <konstruktion@gmail.com>
2015-06-02 21:19:07 +0100

    Updated to Pyblish Integration

--- pyblish_maya/lib.py
+++ pyblish_maya/lib.py
@@ -1,103 +1,25 @@
-"""Internal library for Pyblish Maya
-
-Attributes:
-    cached_process: Temporarily stored Popen instance of Pyblish QML
-    CREATE_NO_WINDOW: Flag from MSDN; see link below.
-    PYBLISH_QML_CONSOLE: Environment variable for displaying
-        the console upon launching Pyblish QML
-
-"""
-
 # Standard library
 import os
-import sys
-import random
 import inspect
-import traceback
-import subprocess
 import contextlib
 
 # Pyblish libraries
 import pyblish.api
+import pyblish_integration
 
 # Host libraries
 from maya import mel
 from maya import cmds
+from maya import utils
 
 # Local libraries
 import plugins
 
-cached_process = None
 
-# https://msdn.microsoft.com/en-us/library/ms684863(v=VS.85).aspx
-CREATE_NO_WINDOW = 0x08000000
-PYBLISH_QML_CONSOLE = "PYBLISH_QML_CONSOLE"
+show = pyblish_integration.show
 
 
-def echo(text):
-    print text
-
-
-def show(console=False, prefer_cached=True):
-    """Show the Pyblish graphical user interface
-
-    An interface may already have been loaded; if that's the
-    case, we favour it to launching a new unless `prefer_cached`
-    is False.
-
-    """
-
-    global cached_process
-
-    if cached_process and prefer_cached:
-        still_running = cached_process.poll() is None
-        if still_running:
-            return _show_cached()
-        else:
-            cached_process = None
-    return _show_new(console)
-
-
-def _show_cached():
-    """Display cached gui
-
-    A GUI is cached upon first being shown, or when pre-loaded.
-
-    """
-
-    import pyblish_endpoint.client
-
-    pyblish_endpoint.client.emit("show")
-
-    return cached_process
-
-
-def _show_new(console=False):
-    """Create and display a new instance of the Pyblish QML GUI"""
-    try:
-        port = os.environ["ENDPOINT_PORT"]
-    except KeyError:
-        raise ValueError("Pyblish start-up script doesn't seem to "
-                         "have been run, could not find the PORT variable")
-
-    pid = os.getpid()
-    kwargs = dict(args=["python", "-m", "pyblish_qml",
-                        "--port", port, "--pid", str(pid)])
-
-    if not console and os.name == "nt":
-        if not os.environ.get(PYBLISH_QML_CONSOLE):
-            kwargs["creationflags"] = CREATE_NO_WINDOW
-
-    echo("Creating a new instance of Pyblish QML")
-    proc = subprocess.Popen(**kwargs)
-
-    global cached_process
-    cached_process = proc
-
-    return proc
-
-
-def setup(preload=True, console=False):
+def setup(console=False):
     """Setup integration
 
     Registers Pyblish for Maya plug-ins and appends an item to the File-menu
@@ -108,74 +30,23 @@ def setup(preload=True, console=False):
 
     """
 
-    if console:
-        os.environ[PYBLISH_QML_CONSOLE] = "1"
+    def threaded_wrapper(func, *args, **kwargs):
+        return utils.executeInMainThreadWithResult(func, *args, **kwargs)
+
+    pyblish_integration.register_dispatch_wrapper(threaded_wrapper)
+    pyblish_integration.setup(console)
 
     register_plugins()
-
-    try:
-        port = setup_endpoint()
-
-        if preload:
-            pid = os.getpid()
-            preload_(port, pid)
-
-    except:
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        message = "".join(traceback.format_exception(
-            exc_type, exc_value, exc_traceback))
-        echo(message)
-        echo("pyblish: Running headless")
-
     add_to_filemenu()
 
-    echo("pyblish: Integration loaded..")
-
-
-def preload_(port, pid=None):
-    pid = os.getpid()
-
-    kwargs = dict(args=["python", "-m", "pyblish_qml",
-                        "--port", str(port), "--pid", str(pid),
-                        "--preload"])
-
-    if os.name == "nt":
-        if not os.environ.get(PYBLISH_QML_CONSOLE):
-            kwargs["creationflags"] = CREATE_NO_WINDOW
-
-    proc = subprocess.Popen(**kwargs)
-
-    global cached_process
-    cached_process = proc
-
-    return proc
-
-
-def setup_endpoint():
-    """Start Endpoint
-
-    Raises:
-        ImportError: If Pyblish Endpoint is not available
-
-    """
-
-    from service import MayaService
-    from pyblish_endpoint import server
-
-    port = find_next_port()
-    server.start_async_production_server(service=MayaService, port=port)
-    os.environ["ENDPOINT_PORT"] = str(port)
-
-    echo("pyblish: Endpoint running @ %i" % port)
-
-    return port
+    pyblish_integration.echo("pyblish: Integration loaded..")
 
 
 def register_plugins():
     # Register accompanying plugins
     plugin_path = os.path.dirname(plugins.__file__)
     pyblish.api.register_plugin_path(plugin_path)
-    echo("pyblish: Registered %s" % plugin_path)
+    pyblish_integration.echo("pyblish: Registered %s" % plugin_path)
 
 
 def add_to_filemenu():
@@ -218,31 +89,7 @@ def _add_to_filemenu():
     # in a deferred call by Maya during idle, it won't have access
     # to other variables declared in this module.
     import pyblish
-
-    try:
-        from pyblish import util
-    except:
-        # Backwards compatibility
-        import pyblish.main as util
-
     import pyblish_maya
-
-    def filemenu_handler(event):
-
-        if event == "publish":
-            try:
-                pyblish_maya.show()
-            except:
-                exc_type, exc_value, exc_traceback = sys.exc_info()
-                message = "".join(traceback.format_exception(
-                    exc_type, exc_value, exc_traceback))
-
-                sys.stderr.write("Tried launching GUI, but failed.\n")
-                sys.stderr.write("Message was:")
-                sys.stderr.write(message)
-                sys.stderr.write("Publishing in headless mode instead.\n")
-
-                util.publish()
 
     cmds.menuItem('pyblishOpeningDivider',
                   divider=True,
@@ -252,15 +99,11 @@ def _add_to_filemenu():
     cmds.menuItem('pyblishScene',
                   insertAfter='pyblishOpeningDivider',
                   label='Publish',
-                  command=lambda _: filemenu_handler("publish"))
+                  command=lambda _: pyblish_maya.show())
 
     cmds.menuItem('pyblishCloseDivider',
                   insertAfter='pyblishScene',
                   divider=True)
-
-
-def find_next_port():
-    return random.randint(6000, 7000)
 
 
 @contextlib.contextmanager
