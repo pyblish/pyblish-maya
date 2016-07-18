8aa2ecd4becd33e94acf9cd09c77f3fe0a30779c
Toke Jepsen <tokejepsen@gmail.com>
2016-07-18 21:49:57 +0100

    Revert "register official Pyblish guis as defaults"
    This reverts commit ed2e1aaa5e1e940504ad9a92e98b1359ac88b3b8.

--- pyblish_maya/pythonpath/userSetup.py
+++ pyblish_maya/pythonpath/userSetup.py
@@ -1,20 +1,12 @@
 
 try:
     __import__("pyblish_maya")
-    __import__("pyblish")
 
 except ImportError as e:
     import traceback
     print ("pyblish-maya: Could not load integration: %s"
            % traceback.format_exc())
+
 else:
-
-    import pyblish.api
     import pyblish_maya
-
-    # Setup integration
     pyblish_maya.setup()
-
-    # register default guis
-    pyblish.api.register_gui("pyblish_qml")
-    pyblish.api.register_gui("pyblish_lite")
