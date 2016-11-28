8f82000ccb74aa753b99e487e7ebe3323d18c57c
Toke Jepsen <tokejepsen@gmail.com>
2016-11-28 10:49:46 +0100

    Upgrade Qt.py to 0.6.8

--- pyblish_maya/vendor/Qt.py
+++ pyblish_maya/vendor/Qt.py
@@ -1,43 +1,172 @@
-"""Map all bindings to PySide2
+"""The MIT License (MIT)
+
+Copyright (c) 2016 Marcus Ottosson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+---
+
+Map all bindings to PySide2
 
 This module replaces itself with the most desirable binding.
 
-Resolution order:
+Project goals:
+    Qt.py was born in the film and visual effects industry to address
+    the growing need for the development of software capable of running
+    with more than one flavour of the Qt bindings for Python - PySide,
+    PySide2, PyQt4 and PyQt5.
+
+    1. Build for one, run with all
+    2. Explicit is better than implicit
+    3. Support co-existence
+
+Default resolution order:
     - PySide2
     - PyQt5
     - PySide
     - PyQt4
 
 Usage:
-    >>> import sys
-    >>> from Qt import QtWidgets
-    >>> app = QtWidgets.QApplication(sys.argv)
-    >>> button = QtWidgets.QPushButton("Hello World")
-    >>> button.show()
-    >>> app.exec_()
+    >> import sys
+    >> from Qt import QtWidgets
+    >> app = QtWidgets.QApplication(sys.argv)
+    >> button = QtWidgets.QPushButton("Hello World")
+    >> button.show()
+    >> app.exec_()
 
 """
 
 import os
 import sys
+import shutil
 
-__version__ = "0.3.2"
+# Flags from environment variables
+QT_VERBOSE = bool(os.getenv("QT_VERBOSE"))                # Extra output
+QT_TESTING = bool(os.getenv("QT_TESTING"))                # Extra constraints
+QT_PREFERRED_BINDING = os.getenv("QT_PREFERRED_BINDING")  # Override default
+
+self = sys.modules[__name__]
+
+# Internal members, may be used externally for debugging
+self.__added__ = list()     # All members added to QtCompat
+self.__remapped__ = list()  # Members copied from elsewhere
+self.__modified__ = list()  # Existing members modified in some way
+
+# Below members are set dynamically on import relative the original binding.
+self.__version__ = "0.6.8"
+self.__qt_version__ = "0.0.0"
+self.__binding__ = "None"
+self.__binding_version__ = "0.0.0"
+
+self.load_ui = lambda fname: None
+self.translate = lambda context, sourceText, disambiguation, n: None
+self.setSectionResizeMode = lambda logicalIndex, hide: None
+
+# All members of this module is directly accessible via QtCompat
+# Take care not to access any "private" members; i.e. those with
+# a leading underscore.
+QtCompat = self
+
+
+def convert(lines):
+    """Convert compiled .ui file from PySide2 to Qt.py
+
+    Arguments:
+        lines (list): Each line of of .ui file
+
+    Usage:
+        >> with open("myui.py") as f:
+        ..   lines = convert(f.readlines())
+
+    """
+
+    def parse(line):
+        line = line.replace("from PySide2 import", "from Qt import")
+        line = line.replace("QtWidgets.QApplication.translate",
+                            "Qt.QtCompat.translate")
+        return line
+
+    parsed = list()
+    for line in lines:
+        line = parse(line)
+        parsed.append(line)
+
+    return parsed
+
+
+def _remap(object, name, value, safe=True):
+    """Prevent accidental assignment of existing members
+
+    Arguments:
+        object (object): Parent of new attribute
+        name (str): Name of new attribute
+        value (object): Value of new attribute
+        safe (bool): Whether or not to guarantee that
+            the new attribute was not overwritten.
+            Can be set to False under condition that
+            it is superseded by extensive testing.
+
+    """
+
+    if QT_TESTING is not None and safe:
+        # Cannot alter original binding.
+        if hasattr(object, name):
+            raise AttributeError("Cannot override existing name: "
+                                 "%s.%s" % (object.__name__, name))
+
+        # Cannot alter classes of functions
+        if type(object).__name__ != "module":
+            raise AttributeError("%s != 'module': Cannot alter "
+                                 "anything but modules" % object)
+
+    elif hasattr(object, name):
+        # Keep track of modifications
+        self.__modified__.append(name)
+
+    self.__remapped__.append(name)
+
+    setattr(object, name, value)
+
+
+def _add(object, name, value):
+    """Append to self, accessible via Qt.QtCompat"""
+    self.__added__.append(name)
+    setattr(object, name, value)
 
 
 def _pyqt5():
     import PyQt5.Qt
+    from PyQt5 import QtCore, QtWidgets, uic
 
-    # Remap
-    PyQt5.QtCore.Signal = PyQt5.QtCore.pyqtSignal
-    PyQt5.QtCore.Slot = PyQt5.QtCore.pyqtSlot
-    PyQt5.QtCore.Property = PyQt5.QtCore.pyqtProperty
+    _remap(QtCore, "Signal", QtCore.pyqtSignal)
+    _remap(QtCore, "Slot", QtCore.pyqtSlot)
+    _remap(QtCore, "Property", QtCore.pyqtProperty)
 
-    # Add
-    PyQt5.__wrapper_version__ = __version__
-    PyQt5.__binding__ = "PyQt5"
-    PyQt5.__binding_version__ = PyQt5.QtCore.PYQT_VERSION_STR
-    PyQt5.__qt_version__ = PyQt5.QtCore.QT_VERSION_STR
-    PyQt5.load_ui = pyqt5_load_ui
+    _add(QtCompat, "__binding__", PyQt5.__name__)
+    _add(QtCompat, "__binding_version__", PyQt5.QtCore.PYQT_VERSION_STR)
+    _add(QtCompat, "__qt_version__", PyQt5.QtCore.QT_VERSION_STR)
+    _add(QtCompat, "load_ui", lambda fname: uic.loadUi(fname))
+    _add(QtCompat, "translate", QtCore.QCoreApplication.translate)
+    _add(QtCompat, "setSectionResizeMode",
+         QtWidgets.QHeaderView.setSectionResizeMode)
+
+    _maintain_backwards_compatibility(PyQt5)
 
     return PyQt5
 
@@ -61,120 +190,165 @@ def _pyqt4():
         raise ImportError
 
     import PyQt4.Qt
+    from PyQt4 import QtCore, QtGui, uic
 
-    # Remap
-    PyQt4.QtWidgets = PyQt4.QtGui
-    PyQt4.QtCore.Signal = PyQt4.QtCore.pyqtSignal
-    PyQt4.QtCore.Slot = PyQt4.QtCore.pyqtSlot
-    PyQt4.QtCore.Property = PyQt4.QtCore.pyqtProperty
-    PyQt4.QtCore.QItemSelection = PyQt4.QtGui.QItemSelection
-    PyQt4.QtCore.QItemSelectionModel = PyQt4.QtGui.QItemSelectionModel
 
-    # Add
-    PyQt4.__wrapper_version__ = __version__
-    PyQt4.__binding__ = "PyQt4"
-    PyQt4.__binding_version__ = PyQt4.QtCore.PYQT_VERSION_STR
-    PyQt4.__qt_version__ = PyQt4.QtCore.QT_VERSION_STR
-    PyQt4.load_ui = pyqt4_load_ui
+    _remap(PyQt4, "QtWidgets", QtGui)
+    _remap(QtCore, "Signal", QtCore.pyqtSignal)
+    _remap(QtCore, "Slot", QtCore.pyqtSlot)
+    _remap(QtCore, "Property", QtCore.pyqtProperty)
+    _remap(QtCore, "QItemSelection", QtGui.QItemSelection)
+    _remap(QtCore, "QStringListModel", QtGui.QStringListModel)
+    _remap(QtCore, "QItemSelectionModel", QtGui.QItemSelectionModel)
+    _remap(QtCore, "QSortFilterProxyModel", QtGui.QSortFilterProxyModel)
+    _remap(QtCore, "QAbstractProxyModel", QtGui.QAbstractProxyModel)
+
+    try:
+        from PyQt4 import QtWebKit
+        _remap(PyQt4, "QtWebKitWidgets", QtWebKit)
+    except ImportError:
+        "QtWebkit is optional in Qt , therefore might not be available"
+
+    _add(QtCompat, "__binding__", PyQt4.__name__)
+    _add(QtCompat, "__binding_version__", PyQt4.QtCore.PYQT_VERSION_STR)
+    _add(QtCompat, "__qt_version__", PyQt4.QtCore.QT_VERSION_STR)
+    _add(QtCompat, "load_ui", lambda fname: uic.loadUi(fname))
+    _add(QtCompat, "setSectionResizeMode", QtGui.QHeaderView.setResizeMode)
+
+    # PySide2 differs from Qt4 in that Qt4 has one extra argument
+    # which is always `None`. The lambda arguments represents the PySide2
+    # interface, whereas the arguments passed to `.translate` represent
+    # those expected of a Qt4 binding.
+    _add(QtCompat, "translate",
+         lambda context, sourceText, disambiguation, n:
+         QtCore.QCoreApplication.translate(context,
+                                           sourceText,
+                                           disambiguation,
+                                           QtCore.QCoreApplication.CodecForTr,
+                                           n))
+
+    _maintain_backwards_compatibility(PyQt4)
 
     return PyQt4
 
 
 def _pyside2():
     import PySide2
-    from PySide2 import QtGui, QtCore
+    from PySide2 import QtGui, QtWidgets, QtCore, QtUiTools
 
-    # Remap
-    QtCore.QStringListModel = QtGui.QStringListModel
+    _remap(QtCore, "QStringListModel", QtGui.QStringListModel)
 
-    # Add
-    PySide2.__wrapper_version__ = __version__
-    PySide2.__binding__ = "PySide2"
-    PySide2.__binding_version__ = PySide2.__version__
-    PySide2.__qt_version__ = PySide2.QtCore.qVersion()
-    PySide2.load_ui = pyside2_load_ui
+    _add(QtCompat, "__binding__", PySide2.__name__)
+    _add(QtCompat, "__binding_version__", PySide2.__version__)
+    _add(QtCompat, "__qt_version__", PySide2.QtCore.qVersion())
+    _add(QtCompat, "load_ui", lambda fname: QtUiTools.QUiLoader().load(fname))
+
+    _add(QtCompat, "setSectionResizeMode",
+         QtWidgets.QHeaderView.setSectionResizeMode)
+
+    _add(QtCompat, "translate", QtCore.QCoreApplication.translate)
+
+    _maintain_backwards_compatibility(PySide2)
 
     return PySide2
 
 
 def _pyside():
     import PySide
-    from PySide import QtGui, QtCore
+    from PySide import QtGui, QtCore, QtUiTools
 
-    # Remap
-    PySide.QtWidgets = QtGui
-    QtCore.QSortFilterProxyModel = QtGui.QSortFilterProxyModel
-    QtCore.QStringListModel = QtGui.QStringListModel
-    PySide.QtCore.QItemSelection = PySide.QtGui.QItemSelection
-    PySide.QtCore.QItemSelectionModel = PySide.QtGui.QItemSelectionModel
+    _remap(PySide, "QtWidgets", QtGui)
+    _remap(QtCore, "QSortFilterProxyModel", QtGui.QSortFilterProxyModel)
+    _remap(QtCore, "QStringListModel", QtGui.QStringListModel)
+    _remap(QtCore, "QItemSelection", QtGui.QItemSelection)
+    _remap(QtCore, "QItemSelectionModel", QtGui.QItemSelectionModel)
+    _remap(QtCore, "QAbstractProxyModel", QtGui.QAbstractProxyModel)
 
-    # Add
-    PySide.__wrapper_version__ = __version__
-    PySide.__binding__ = "PySide"
-    PySide.__binding_version__ = PySide.__version__
-    PySide.__qt_version__ = PySide.QtCore.qVersion()
-    PySide.load_ui = pyside_load_ui
+    try:
+        from PySide import QtWebKit
+        _remap(PySide, "QtWebKitWidgets", QtWebKit)
+    except ImportError:
+        "QtWebkit is optional in Qt, therefore might not be available"
+
+    _add(QtCompat, "__binding__", PySide.__name__)
+    _add(QtCompat, "__binding_version__", PySide.__version__)
+    _add(QtCompat, "__qt_version__", PySide.QtCore.qVersion())
+    _add(QtCompat, "load_ui", lambda fname: QtUiTools.QUiLoader().load(fname))
+    _add(QtCompat, "setSectionResizeMode", QtGui.QHeaderView.setResizeMode)
+
+    _add(QtCompat, "translate",
+         lambda context, sourceText, disambiguation, n:
+         QtCore.QCoreApplication.translate(context,
+                                           sourceText,
+                                           disambiguation,
+                                           QtCore.QCoreApplication.CodecForTr,
+                                           n))
+
+    _maintain_backwards_compatibility(PySide)
 
     return PySide
 
 
-def pyside_load_ui(fname):
-    """Read Qt Designer .ui `fname`
-
-    Args:
-        fname (str): Absolute path to .ui file
-
-    Usage:
-        >> from Qt import load_ui
-        >> class MyWindow(QtWidgets.QWidget):
-        ..   fname = 'my_ui.ui'
-        ..   self.ui = load_ui(fname)
-        ..
-        >> window = MyWindow()
-
-    """
-
-    from PySide import QtUiTools
-    return QtUiTools.QUiLoader().load(fname)
+def _log(text, verbose):
+    if verbose:
+        sys.stdout.write(text + "\n")
 
 
-def pyside2_load_ui(fname):
-    """Read Qt Designer .ui `fname`
+def cli(args):
+    """Qt.py command-line interface"""
+    import argparse
 
-    Args:
-        fname (str): Absolute path to .ui file
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--convert",
+                        help="Path to compiled Python module, e.g. my_ui.py")
+    parser.add_argument("--compile",
+                        help="Accept raw .ui file and compile with native "
+                             "PySide2 compiler.")
+    parser.add_argument("--stdout",
+                        help="Write to stdout instead of file",
+                        action="store_true")
+    parser.add_argument("--stdin",
+                        help="Read from stdin instead of file",
+                        action="store_true")
 
-    """
+    args = parser.parse_args(args)
 
-    from PySide2 import QtUiTools
-    return QtUiTools.QUiLoader().load(fname)
+    if args.stdout:
+        raise NotImplementedError("--stdout")
+
+    if args.stdin:
+        raise NotImplementedError("--stdin")
+
+    if args.compile:
+        raise NotImplementedError("--compile")
+
+    if args.convert:
+        sys.stdout.write("#\n"
+                         "# WARNING: --convert is an ALPHA feature.\n#\n"
+                         "# See https://github.com/mottosso/Qt.py/pull/132\n"
+                         "# for details.\n"
+                         "#\n")
+
+        #
+        # ------> Read
+        #
+        with open(args.convert) as f:
+            lines = convert(f.readlines())
+
+        backup = "%s_backup%s" % os.path.splitext(args.convert)
+        sys.stdout.write("Creating \"%s\"..\n" % backup)
+        shutil.copy(args.convert, backup)
+
+        #
+        # <------ Write
+        #
+        with open(args.convert, "w") as f:
+            f.write("".join(lines))
+
+        sys.stdout.write("Successfully converted \"%s\"\n" % args.convert)
 
 
-def pyqt4_load_ui(fname):
-    """Read Qt Designer .ui `fname`
-
-    Args:
-        fname (str): Absolute path to .ui file
-
-    """
-
-    from PyQt4 import uic
-    return uic.loadUi(fname)
-
-
-def pyqt5_load_ui(fname):
-    """Read Qt Designer .ui `fname`
-
-    Args:
-        fname (str): Absolute path to .ui file
-
-    """
-
-    from PyQt5 import uic
-    return uic.loadUi(fname)
-
-
-def _init():
+def init():
     """Try loading each binding in turn
 
     Please note: the entire Qt module is replaced with this code:
@@ -183,55 +357,90 @@ def _init():
     This means no functions or variables can be called after
     this has executed.
 
+    For debugging and testing, this module may be accessed
+    through `Qt.__shim__`.
+
     """
 
-    preferred = os.getenv("QT_PREFERRED_BINDING")
-    verbose = os.getenv("QT_VERBOSE") is not None
+    bindings = (_pyside2, _pyqt5, _pyside, _pyqt4)
 
-    if preferred:
-
-        # Debug mode, used in installer
-        if preferred == "None":
-            sys.modules[__name__].__wrapper_version__ = __version__
+    if QT_PREFERRED_BINDING:
+        # Internal flag (used in installer)
+        if QT_PREFERRED_BINDING == "None":
+            self.__wrapper_version__ = self.__version__
             return
 
+        preferred = QT_PREFERRED_BINDING.split(os.pathsep)
         available = {
             "PySide2": _pyside2,
-            "PySide": _pyside,
             "PyQt5": _pyqt5,
+            "PySide": _pyside,
             "PyQt4": _pyqt4
         }
 
-        if preferred not in available:
-            raise ImportError("Preferred Qt binding \"%s\" "
-                              "not available" % preferred)
+        try:
+            bindings = [available[binding] for binding in preferred]
+        except KeyError:
+            raise ImportError(
+                "Available preferred Qt bindings: "
+                "\n".join(preferred)
+            )
 
-        binding = available[preferred]
-        sys.modules[__name__] = binding()
-        return
+    for binding in bindings:
+        _log("Trying %s" % binding.__name__, QT_VERBOSE)
 
-    else:
-        for binding in (_pyside2,
-                        _pyqt5,
-                        _pyside,
-                        _pyqt4):
+        try:
+            binding = binding()
 
-            if verbose:
-                sys.stdout.write("Trying %s" % binding.__name__[1:])
+        except ImportError as e:
+            _log(" - ImportError(\"%s\")" % e, QT_VERBOSE)
+            continue
 
-            try:
-                sys.modules[__name__] = binding()
-                return
+        else:
+            # Reference to this module
+            binding.QtCompat = self
+            binding.__shim__ = self  # DEPRECATED
 
-            except ImportError as e:
+            sys.modules.update({
+                __name__: binding,
 
-                if verbose:
-                    sys.stdout.write(" - ImportError(\"%s\")\n" % e)
+                # Fix #133, `from Qt.QtWidgets import QPushButton`
+                __name__ + ".QtWidgets": binding.QtWidgets,
 
-                continue
+                # Fix #158 `import Qt.QtCore;Qt.QtCore.Signal`
+                __name__ + ".QtCore": binding.QtCore,
+                __name__ + ".QtGui": binding.QtGui,
+
+            })
+
+            return
 
     # If not binding were found, throw this error
     raise ImportError("No Qt binding were found.")
 
 
-_init()
+def _maintain_backwards_compatibility(binding):
+    """Add members found in prior versions up till the next major release
+
+    These members are to be considered deprecated. When a new major
+    release is made, these members are removed.
+
+    """
+
+    for member in ("__binding__",
+                   "__binding_version__",
+                   "__qt_version__",
+                   "__added__",
+                   "__remapped__",
+                   "__modified__",
+                   "convert",
+                   "load_ui",
+                   "translate"):
+        setattr(binding, member, getattr(self, member))
+        self.__added__.append(member)
+
+    setattr(binding, "__wrapper_version__", self.__version__)
+    self.__added__.append("__wrapper_version__")
+
+
+cli(sys.argv[1:]) if __name__ == "__main__" else init()
